0455b5263da486747ef1b0e52ecaa574e599cb5e
mao13132 <mao13132@gmail.com>
2024-02-17 20:09:59 +0300

    добавил анимацию

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
+import { useState } from 'react';
+
+import { CSSTransition } from 'react-transition-group';
+
 function App() {
+
+
+  const [open, setOpen] = useState(false);
+
+  const clickOpen = () => {
+    setOpen(true);
+  };
+
+  const clickClose = () => {
+    setOpen(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={clickOpen} className='open-modal-btn'>Открыть окно</button>
+
+
+      <CSSTransition in={open} timeout={500} classNames='animated' unmountOnExit>
+        <div className="overlay">
+          <div className="modal">
+            <img onClick={clickClose} className='plus' src="./plus.svg" alt="закрыть" />
+            <img className='content' src="https://media2.giphy.com/media/v1.Y2lkPTc5MGI3NjExdGQ5MGpwMHU3dzR6OHIyN2ZpbWxqdWg3amk1dm85MmEzNjJ0M2lndiZlcD12MV9naWZzX3NlYXJjaCZjdD1n/2zZNmrvguyPTS06rK0/200.gif" alt="содерэание" />
+          </div>
+        </div>
+      </CSSTransition>
+
+
     </div>
   );
 }
